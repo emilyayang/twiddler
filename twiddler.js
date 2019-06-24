2ba337c67b89197bbdc7e8369cf924742f122da2
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-06-24 10:13:32 -0700

    "first commit never went through, work completed so far"

--- twiddler.js
+++ twiddler.js
@@ -1,59 +1,80 @@
 $(document).ready(function(){
-  var $body = $('body');
+//   Creates two global variables, users and streams.
+// users is an array of strings -- all the usernames that you're following.
+// streams is an object with two properties, users and home.
+// streams.home is an array of all tweets from all the users you're following.
+// streams.users is an object with properties for each user. streams.users.shawndrost has all of shawndrost's tweets.
+// Kicks off a periodic process that puts more data in streams.
+    var $body = $('body');
   // $body.html('');
-
   var index = streams.home.length - 1; //an array of all tweets from all the users you're following
+  var totalTweet = streams.home.length - 1; //num of total tweets
   while(index >= 0){
-    var tweetTime = streams.home[index].created_at;
     var tweet = streams.home[index];
-    var $tweet = $('<div class="tweets"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + '. ' + tweetTime);
+
+    var $username = $("<a class ='username' href='#'></a>"); //creates div for username
+    $username.text('@' + tweet.user + ': ' + '\n');
+
+    var $tweet = $('<div class="tweets"></div><br>'); // creates an individual div for every tweet
+    $tweet.append($username);
+
+    var $message = $("<div></div>");
+    $message.text(tweet.message + '. ' + tweet.created_at);
+    $tweet.append($message);
+
+    $tweet.text('@' + tweet.user + ': '  +  tweet.message + '. ' + tweet.created_at);
     $tweet.appendTo($body);
     index -= 1;
   }
 
   $('#showNewTweets').on('click', function(){
-    var $newTweet = $('<div class="tweets"></div>');
-    var randomUser = randomElement(users);
-    var randomMess = randomMessage();
-    var newTweet = '@' + randomUser + ': ' + randomMess + '. ' + tweetTime; //tweetTime not up to date
-    $newTweet.text(newTweet);
-    $newTweet.appendTo($body);
-    addTweet(newTweet);
-  })
-});
-
-
-
-// Create a new tweet !!! NOT WORKING !!! 
-$("#submit").on('click', function(){
-  var message = $("#tweetText").val();
-  writeTweet(message);
-  displayTweets();
-});
-  
-
-
-
-  //   var checkTweets = true;
-  //   $(document).ready(function(){
-  //     $('#showNewTweets').on('click', function(){
-  //       if(checkTweets){
-  //         var index = streams.home.length - 1;
-  //         while (index <= streams.home.length - 1){
-  //           var tweetTime = streams.home[index].created_at;
-  //           var tweet = streams.home[index];
-  //           var $tweet = $('<div></div>');
-  //           $tweet.text('@' + tweet.user + ': ' + tweet.message + '. ' + tweetTime);
-  //           $tweet.appendTo($body);
-  //           index -= 1;
-  //           checkTweets = false;
-  //         }
-  //       } else {
-  //         $('#showNewTweets').empty();
-  //         checkTweets = false;
-  //       }
-  //     })
-  //   });
+    var latestIndex = streams.home.length-1;
+    var tweet = streams.home[latestIndex];
     
+    var $username = $("<a class ='username' href='#'></a>");
+    $username.text('@' + tweet.user + ': ' );
+    
+    var $tweet = $('<div class="tweets"></div><br>'); // creates an individual div for every tweet
+    $tweet.append($username);
+
+    var $message = $("<div></div>");
+    $message.text(tweet.message + '. ' + tweet.created_at);
+    $tweet.append($message);
+
+    // $tweet.text('@' + tweet.user + ': ' + tweet.message + '. ' + tweet.created_at);
+    $tweet.insertBefore(".tweets:first");
+
+    console.log('Made it to username onlcick');
+$(".username").on("click", function(){
+    console.log('Username was just clicked');
+    $("side").html("<div class='side'></div>"); //create div for side panel
+    var username = this.textContent;
+    var name = username.slice(1, username.length-2);
+    $("side").append(name);
+    var index = streams.users[name].length - 1;
+    while(index >= 0){
+      var userTweet = streams.users[name][index];
+      console.log(userTweet);
+      var $userTweet = $("<div class ='side-tweet'></div>");
+      $userTweet.text(userTweet.message + ', ' + userTweet.created_at);
+      $userTweet.appendTo(".side");
+      index -= 1; 
+    }
+  });
+  });
+  
+});
+
+
+
+  // $('#showNewTweets').on('click', function(){
+  //   var index = streams.home.length - 1;
+  //   while (index > totalTweet){
+  //     var tweet = streams.home[index];
+  //     var $tweet = $('<div class="indiv-tweets"></div>');
+  //     $tweet.text('@' + tweet.user + ': ' + tweet.message + '. ' + tweetTime);
+  //     $tweet.appendTo($body);
+  //     index -= 1;
+  //   }
+  //   totalTweets = streams.home.length;
   // });
